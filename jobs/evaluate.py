a431232603ddb82bb6c2e419681c2fa56ae43c83
tom <tab242@cornell.edu>
2017-11-30 10:36:14 -0600

    Remove XGB

--- jobs/evaluate.py
+++ jobs/evaluate.py
@@ -9,41 +9,9 @@ from keras.models import load_model
 from config import DATA_ROOT
 
 from jobs.preprocess import PreprocessRawData
-from jobs.xgb import XGBFitModel
 from jobs.cnn import KerasCNNFitModel
 
 from util import evaluate
-
-
-class Evaluate(luigi.Task):
-    """
-    Calculate metrics on the final fit XGBoost model
-    """
-
-    def requires(self):
-        return [
-            XGBFitModel(),
-            PreprocessRawData(),
-        ]
-
-    def output(self):
-        return luigi.LocalTarget(os.path.join(DATA_ROOT, 'evaluation.json'))
-
-    def run(self):
-        with self.input()[0].open('r') as path_file:
-            paths = json.load(path_file)
-
-        proba_predictions = np.load(paths['proba_predictions'])
-
-        with self.input()[1].open('r') as path_file:
-            paths = json.load(path_file)
-
-        y_test = np.load(paths['y_test'])
-
-        evaluation = evaluate.evaluate_output(proba_predictions, y_test)
-
-        with self.output().open('w') as f:
-            json.dump(evaluation, f)
 
 
 class EvaluateCNN(luigi.Task):
