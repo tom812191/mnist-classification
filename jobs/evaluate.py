d34cbfb5d64a38f90fbb8fea0251e1e7325250e1
tom <tab242@cornell.edu>
2017-11-30 07:57:12 -0600

    Add cnn evaluator

--- jobs/evaluate.py
+++ jobs/evaluate.py
@@ -3,10 +3,13 @@ import os
 import numpy as np
 import json
 
+from keras.models import load_model
+
 from config import DATA_ROOT
 
 from jobs.preprocess import PreprocessRawData
 from jobs.xgb import XGBFitModel
+from jobs.cnn import KerasCNNFitModel
 
 from util import evaluate
 
@@ -29,7 +32,6 @@ class Evaluate(luigi.Task):
         with self.input()[0].open('r') as path_file:
             paths = json.load(path_file)
 
-        predictions = np.load(paths['predictions'])
         proba_predictions = np.load(paths['proba_predictions'])
 
         with self.input()[1].open('r') as path_file:
@@ -37,7 +39,41 @@ class Evaluate(luigi.Task):
 
         y_test = np.load(paths['y_test'])
 
-        evaluation = evaluate.evaluate_output(predictions, proba_predictions, y_test)
+        evaluation = evaluate.evaluate_output(proba_predictions, y_test)
+
+        with self.output().open('w') as f:
+            json.dump(evaluation, f)
+
+
+class EvaluateCNN(luigi.Task):
+    """
+    Calculate metrics on the final fit CNN model
+    """
+
+    def requires(self):
+        return [
+            KerasCNNFitModel(),
+            PreprocessRawData(),
+        ]
+
+    def output(self):
+        return luigi.LocalTarget(os.path.join(DATA_ROOT, 'evaluation.json'))
+
+    def run(self):
+        with self.input()[0].open('r') as path_file:
+            paths = json.load(path_file)
+
+        model = load_model(paths['model'])
+
+        with self.input()[1].open('r') as path_file:
+            paths = json.load(path_file)
+
+        X_test = np.load(paths['X_test'])
+        y_test = np.load(paths['y_test'])
+
+        proba_predictions = model.predict(X_test)
+
+        evaluation = evaluate.evaluate_output(proba_predictions, y_test)
 
         with self.output().open('w') as f:
             json.dump(evaluation, f)
--- util/evaluate.py
+++ util/evaluate.py
@@ -1,16 +1,18 @@
 from sklearn.metrics import log_loss, accuracy_score, confusion_matrix
+import numpy as np
 
 
-def evaluate_output(predictions, proba_predictions, y_test):
+def evaluate_output(proba_predictions, y_test):
     """
     Evaluate the predictions based on accuracy, log loss, and confusion
 
-    :param predictions: np.array of class predictions
     :param proba_predictions: np.array of probability of class predictions
     :param y_test: np.array of true y_test labels
 
     :return: dict with keys as the metrics and values as the results
     """
+    predictions = np.argmax(proba_predictions, axis=1)
+
     return {
         'accuracy_score': accuracy_score(y_test, predictions),
         'log_loss': log_loss(y_test, proba_predictions),
